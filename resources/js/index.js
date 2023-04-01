dfc4adee9a5e4e323bf83de8516dc9d3fb1124db
jefflangtech <jeff.m.lang@gmail.com>
2023-04-01 11:12:59 -0700

    Created slide functionality using transition for translateX and bypassing the transiton while moving grid items

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,25 +2,65 @@ const carousel = document.getElementsByClassName('carousel-inner');
 const carouselItems = document.getElementsByClassName('carousel-image');
 const leftBtn = document.getElementById('left-carousel-button');
 const rightBtn = document.getElementById('right-carousel-button');
+const root = document.documentElement;
+const rootStyle = getComputedStyle(root);
+const transitionTiming = rootStyle.getPropertyValue('--transition-delay').slice(0, -1);
 
 const items = [];
 for(let i = 0; i < carouselItems.length; i++) {
   items.push(carouselItems[i]);
 }
-// let imgWidth = items[0].clientWidth;
+let imgWidth = items[0].clientWidth;
 
-leftBtn.addEventListener('click', () => {
+const carouselShiftLeft = function() {
+  leftBtn.removeEventListener('click', carouselShiftLeft);
+  rightBtn.removeEventListener('click', carouselShiftRight);
+
+  items.forEach(item => {
+    item.style.transform = `translateX(-${imgWidth}px)`;
+  });
   let firstItem = items.shift();
   items.push(firstItem);
-  items.forEach((item, index) => {
-    item.style.gridColumn = `${index + 1}`;
+  setTimeout(() => {
+    items.forEach(item => {
+      item.style.setProperty('transition-duration', '0s');
+      item.style.transform = `translateX(0px)`;
+    });
+    items.forEach((item, index) => {
+      item.style.gridColumn = `${index + 1}`;
+    });
+    leftBtn.addEventListener('click', carouselShiftLeft);
+    rightBtn.addEventListener('click', carouselShiftRight);
+  }, (parseInt(transitionTiming) * 1000));
+  items.forEach(item => {
+    item.style.setProperty('transition-duration', `${transitionTiming}s`);
   });
-});
+}
 
-rightBtn.addEventListener('click', () => {
+const carouselShiftRight = function() {
+  leftBtn.removeEventListener('click', carouselShiftLeft);
+  rightBtn.removeEventListener('click', carouselShiftRight);
+
+  items.forEach(item => {
+    item.style.transform = `translateX(${imgWidth}px)`;
+  });
   let lastItem = items.pop();
   items.unshift(lastItem);
-  items.forEach((item, index) => {
-    item.style.gridColumn = `${index + 1}`;
+  setTimeout(() => {
+    items.forEach(item => {
+      item.style.setProperty('transition-duration', '0s');
+      item.style.transform = `translateX(0px)`;
+    });
+    items.forEach((item, index) => {
+      item.style.gridColumn = `${index + 1}`;
+    });
+    leftBtn.addEventListener('click', carouselShiftLeft);
+    rightBtn.addEventListener('click', carouselShiftRight);
+  }, (parseInt(transitionTiming) * 1000));
+  items.forEach(item => {
+    item.style.setProperty('transition-duration', `${transitionTiming}s`);
   });
-});
+}
+
+leftBtn.addEventListener('click', carouselShiftLeft);
+rightBtn.addEventListener('click', carouselShiftRight);
