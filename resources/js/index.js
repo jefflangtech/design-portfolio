bd872971ef77963c72de7f0a2ba98671496367f1
jefflangtech <jeff.m.lang@gmail.com>
2023-04-01 18:19:22 -0700

    Finished the carousel function with some refactoring

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,17 +1,42 @@
-const carousel = document.getElementsByClassName('carousel-inner');
+// Get the elements necessary to run the carousel
 const carouselItems = document.getElementsByClassName('carousel-image');
 const leftBtn = document.getElementById('left-carousel-button');
 const rightBtn = document.getElementById('right-carousel-button');
+// Capture the transition time from the CSS file (custom variable)
 const root = document.documentElement;
 const rootStyle = getComputedStyle(root);
 const transitionTiming = rootStyle.getPropertyValue('--transition-delay').slice(0, -1);
 
+// Load the carousel items into an arry
 const items = [];
 for(let i = 0; i < carouselItems.length; i++) {
   items.push(carouselItems[i]);
 }
+// Capture the width of a carousel item for the shift
 let imgWidth = items[0].clientWidth;
 
+// After the slide, this function processes an instant shift back
+// and a re-organization of the grid items so as to be in the place
+// of the items after the slide
+const gridItemShift = function() {
+  setTimeout(() => {
+    items.forEach(item => {
+      item.style.setProperty('transition-duration', '0s');
+      item.style.transform = `translateX(0px)`;
+    });
+    // Reorganizes the grid items based on their array index
+    items.forEach((item, index) => {
+      item.style.gridColumn = `${index + 1}`;
+    });
+    leftBtn.addEventListener('click', carouselShiftLeft);
+    rightBtn.addEventListener('click', carouselShiftRight);
+  }, (parseFloat(transitionTiming) * 1000));
+  items.forEach(item => {
+    item.style.setProperty('transition-duration', `${transitionTiming}s`);
+  });
+}
+
+// Slides the carousel left and changes item array order to match
 const carouselShiftLeft = function() {
   leftBtn.removeEventListener('click', carouselShiftLeft);
   rightBtn.removeEventListener('click', carouselShiftRight);
@@ -21,22 +46,11 @@ const carouselShiftLeft = function() {
   });
   let firstItem = items.shift();
   items.push(firstItem);
-  setTimeout(() => {
-    items.forEach(item => {
-      item.style.setProperty('transition-duration', '0s');
-      item.style.transform = `translateX(0px)`;
-    });
-    items.forEach((item, index) => {
-      item.style.gridColumn = `${index + 1}`;
-    });
-    leftBtn.addEventListener('click', carouselShiftLeft);
-    rightBtn.addEventListener('click', carouselShiftRight);
-  }, (parseInt(transitionTiming) * 1000));
-  items.forEach(item => {
-    item.style.setProperty('transition-duration', `${transitionTiming}s`);
-  });
+
+  gridItemShift();
 }
 
+// Slides the carousel right and changes item array order to match
 const carouselShiftRight = function() {
   leftBtn.removeEventListener('click', carouselShiftLeft);
   rightBtn.removeEventListener('click', carouselShiftRight);
@@ -46,20 +60,8 @@ const carouselShiftRight = function() {
   });
   let lastItem = items.pop();
   items.unshift(lastItem);
-  setTimeout(() => {
-    items.forEach(item => {
-      item.style.setProperty('transition-duration', '0s');
-      item.style.transform = `translateX(0px)`;
-    });
-    items.forEach((item, index) => {
-      item.style.gridColumn = `${index + 1}`;
-    });
-    leftBtn.addEventListener('click', carouselShiftLeft);
-    rightBtn.addEventListener('click', carouselShiftRight);
-  }, (parseInt(transitionTiming) * 1000));
-  items.forEach(item => {
-    item.style.setProperty('transition-duration', `${transitionTiming}s`);
-  });
+
+  gridItemShift();
 }
 
 leftBtn.addEventListener('click', carouselShiftLeft);
